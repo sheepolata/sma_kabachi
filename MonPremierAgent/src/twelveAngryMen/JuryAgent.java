c993c0163f5f6db33b5bbed7f7a879897d11c311
Antoine Gaget <antoine.gaget@gmail.com>
2016-10-25 12:00:02 +0200

    ajout du fichiers pour la classe d'agent 12 Hommes en Colere

---
+++ MonPremierAgent/src/twelveAngryMen/JuryAgent.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twelveAngryMen;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+import java.util.*;
+
+/**
+ *
+ * @author sheep
+ */
+public class JuryAgent extends Agent{
+    
+}
